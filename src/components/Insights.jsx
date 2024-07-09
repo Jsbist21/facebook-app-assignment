45b343c43b1bdf33938bfdfefe9454ef1f72e4ae
Jsbist21 <bishtjatin477@gmail.com>
2024-07-09 13:02:30 +0530

    bug fixes in insights

--- src/components/Insights.jsx
+++ src/components/Insights.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import moment from "moment";
 const Insights = ({ pageId, accessToken }) => {
   const [insights, setInsights] = useState({});
   const [since, setSince] = useState("2023-01-01");
@@ -14,15 +14,17 @@ const Insights = ({ pageId, accessToken }) => {
     const fetchInsights = async () => {
       setLoading(true);
       setError(null);
+      const formattedSince = moment(since).format("YYYY-MM-DD");
+      const formattedUntil = moment(until).format("YYYY-MM-DD");
       try {
         const result = await axios.get(
           `https://graph.facebook.com/${pageId}/insights`,
           {
             params: {
               metric:
-                "page_follows,post_engaged_users,page_impressions,post_reactions_by_type_total",
-              since,
-              until,
+                "page_follows,page_post_engagements,page_impressions,post_reactions_by_type_total",
+              formattedSince,
+              formattedUntil,
               period: "day",
               access_token: accessToken,
             },
@@ -75,7 +77,7 @@ const Insights = ({ pageId, accessToken }) => {
       {!loading && !error && (
         <>
           <div>Followers: {insights.page_follows}</div>
-          <div>Engagement: {insights.post_engaged_users}</div>
+          <div>Engagement: {insights.page_post_engagements}</div>
           <div>Impressions: {insights.page_impressions}</div>
           <div>Reactions: {insights.post_reactions_by_type_total}</div>
         </>
